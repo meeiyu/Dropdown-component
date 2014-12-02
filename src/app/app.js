be03261c5bb46fe0b425bcd0b2e25e12dd6f7369
Mandy Pao <Mandy_Pao@M82-201403B.migosoft.com>
2014-12-02 10:40:30 +0800

    1. Add basic style. (main.css) 2. Add renderOptions (BSDropdowns.js) 3. Add example (app.js)

--- src/app/app.js
+++ src/app/app.js
@@ -3,14 +3,31 @@
 'use strict';
 
 var React = require('react'),
-    ExampleApp;
+    BSDropdowns = require('./components/BSDropdowns'),
+    ExampleApp,
+    DropdownSelectOptions1 = [
+        {id:1, text:'This is Select1', href:'index1', selected: true, description:'Description', imageSrc:''},
+        {id:2, text:'Select2', href:'index2', selected: false, description:'', imageSrc:'http://dl.dropbox.com/u/40036711/Images/facebook-icon-32.png'},
+        {id:3, text:'', href:'index3', selected: false, description:'Description', imageSrc:'http://dl.dropbox.com/u/40036711/Images/facebook-icon-32.png'}
+    ],
+    DropdownSelectOptions2 = [
+        {id:1, text:'This is Select4', href:'index4', selected: false, description:'Description', imageSrc:'http://dl.dropbox.com/u/40036711/Images/facebook-icon-32.png'},
+        {id:2, text:'Select5', href:'index5', selected: false, description:'Description', imageSrc:'http://dl.dropbox.com/u/40036711/Images/facebook-icon-32.png'},
+        {id:3, text:'Select6', href:'index6', selected: false, description:'Description', imageSrc:'http://dl.dropbox.com/u/40036711/Images/facebook-icon-32.png'}
+    ];
 
 ExampleApp = React.createClass({
     render: function() {
         return (
-        	/*jshint ignore:start */
+            /*jshint ignore:start */
             <div>
-            	<h2>Hello, World</h2>
+                <BSDropdowns 
+                    name="dropdown"
+                    options={DropdownSelectOptions1} />
+
+                <BSDropdowns 
+                    name="dropdown2"
+                    options={DropdownSelectOptions2} />
             </div>
             /*jshint ignore:end */
         );
---
+++ src/app/components/BSDropdowns.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/*
+ * BSDropdown
+ */
+
+var React = require('react'),
+    BSDropdowns;
+
+BSDropdowns = React.createClass({
+    propTypes: {
+        text            : React.PropTypes.string.isRequired,
+        options         : React.PropTypes.array.isRequired,
+        onChangeHandler : React.PropTypes.func
+    },
+    getDefaultProps: function() {
+        return {
+            text            : '',
+            options         : [],
+            onChangeHandler : function() {}
+        };
+    },
+    getInitialState: function() {
+        return {
+            optionsList     : this.props.options
+        };
+    },
+    renderOptions: function() {
+        return this.state.optionsList.map(function(item, i) {
+            var className = (item.selected === true) ? 'disabled' : ' ';
+            return (
+                /* jshint ignore:start */
+                <li key={i} value={item.id} role="presentation" className={className}>
+                    <a role="menuitem" className="dropdown-option" href={item.href}>
+                        <img className="dropdown-img" src={item.imageSrc}></img>
+                        <span className="dropdown-text">{item.text}</span>
+                        <span className="dropdown-desc">{item.description}</span>
+                    </a>
+                </li>
+                /* jshint ignore:end */
+            );
+        });
+    },
+    render: function() {
+        return (
+            /* jshint ignore:start */
+            <div className="dropdown">
+                <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+                    Dropdown
+                    <span className="caret"></span>
+                </button>
+                <ul className="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1" name={this.props.text}>
+                    {this.renderOptions()}
+                </ul>
+            </div>
+            /* jshint ignore:end */
+        );
+    }
+});
+
+module.exports = BSDropdowns;
